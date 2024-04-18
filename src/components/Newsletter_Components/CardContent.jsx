2861e10e570108c9c13c514326697d903e7ff70f
Abhijit Tambade <abhijittambade03@gmail.com>
2024-04-18 23:53:51 +0530

    resized newsletter card

--- src/components/Newsletter_Components/CardContent.jsx
+++ src/components/Newsletter_Components/CardContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CardContent(props) {
     return (
         <div className="article-content">
-            <h2>{props.heading}</h2>
+            <h5>{props.heading}</h5>
             <p>{props.description}</p>
         </div>
     );
