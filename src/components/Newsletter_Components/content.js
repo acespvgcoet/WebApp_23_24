9459932e69fe2bf9e8da129d949785c09c20ffad
Abhijit Tambade <abhijittambade03@gmail.com>
2024-04-20 09:06:32 +0530

    added blockchain newsletter

--- src/components/Newsletter_Components/content.js
+++ src/components/Newsletter_Components/content.js
@@ -1,11 +1,19 @@
 const content = [
     {
         key: 1,
+        card_img: require('./Newsletter Images/blockchain.jpg'),
+        edition: "Mar 2023",
+        heading: "Blockchain: The Security Future",
+        description: "In an increasingly digital world, security is paramount. As data breaches and cyber threats become more...",
+        pdf_src: require("./Newsletter PDFs/Newsletter1.pdf")
+    },
+    {
+        key: 2,
         card_img: require('./Newsletter Images/dsa.png'),
         edition: "Oct 2023",
         heading: "Data Structures: Beyond the Books",
-        description: "Understanding the real-world applications of data structures is crucial for tech enthusiasts for seve.....",
-        pdf_src: require("./Newsletter PDFs/Newsletter1.pdf")
+        description: "Understanding the real-world applications of data structures is crucial for tech enthusiasts for seve...",
+        pdf_src: require("./Newsletter PDFs/Newsletter2.pdf")
     }
 ];
 
